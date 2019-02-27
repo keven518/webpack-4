f970307a062cfe4a5fe17acc47a00a51d708ec34
KEWEN <kewen@huazhu.com>
2019-02-27 10:18:07 +0800

    init

---
+++ readme.txt
@@ -0,0 +1,7 @@
+https://malun666.github.io/aicoder_vip_doc/#/pages/vip_2webpack
+
+cnpm i webpack -D
+cnpm i webpack-cli -D
+
+css-loader 支持 import './style/index.css'; 
+style-loader 在main.js中生成style标签注入到index.html中
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import './style/index.css';  // loader=> css-loader module, style-loader
+
+function createDomElemete() {
+  let dom = document.createElement('div');
+  dom.innerHTML = _.join(['aicoder.com', ' 好! ', '线下实习'], '');
+  // dom.className = 'box';
+  dom.classList.add('box');
+  return dom;
+}
+
+let divDom = createDomElemete();
+
+document.body.appendChild(divDom);
